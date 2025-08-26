33336885946384fcfbb6bd3a82ebdd1b7479a291
juliocesarr1p <00001146384233sp@al.educacao.sp.gov.br>
2025-08-26 15:40:13 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarCuriosidade() {
+    const curiosidade = "Neymar quase jogou futsal profissionalmente antes de se tornar estrela no futebol de campo!";
+    document.getElementById("curiosidade").innerText = curiosidade;
+}
